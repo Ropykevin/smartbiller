da64bd0b135441e940bdc11c3991920b8deec987
ropykevin <ropykevin@gmail.com>
2025-07-05 16:49:07 -0900

    first commit

--- deployment.sh
+++ deployment.sh
@@ -1,8 +1,4 @@
-#!/bin/bash
-
 sh mypostgresql.sh
 git pull origin master
 docker-compose -f docker-compose.yml up -d --build 
-
-# Optional: view logs for the main web service
-docker logs -f smartbiller-web
+docker logs -f smartbiller
